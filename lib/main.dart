f8195e33fb7f0db1b5de082af62bdaa9ff97ab50
Tan <tanho2692002@gmail.com>
2023-03-09 15:25:55 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: HHH(),
     );
